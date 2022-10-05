2c6eefdb6643b5815de3994e66ee56d75bd66b19
DungNV <dungnv4989@gmail.com>
2022-10-05 14:02:15 +0700

    add push file

---
+++ uploadFilesAPI/TestUpload/Form1.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestUpload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_upload_Click(object sender, EventArgs e)
+        {
+            var API_UPLOAD = "http://localhost:45320/api/Home/UploadFile";
+
+            using(var dlg = new OpenFileDialog())
+            {
+                dlg.Multiselect = true;
+                if(dlg.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (var filename in dlg.FileNames)
+                    {
+                        var client = new WebClient();
+                        client.Headers.Add("Content-Type", "binary/octet-stream");                        
+                        client.UploadFileAsync(new Uri(API_UPLOAD), filename);                      
+                        client.UploadFileCompleted += Client_UploadFileCompleted;
+                        client.UploadProgressChanged += Client_UploadProgressChanged;
+                    }
+                    
+                }
+            }
+        }
+
+        private void Client_UploadProgressChanged(object sender, UploadProgressChangedEventArgs e)
+        {
+            progressBar1.BeginInvoke(new Action(() => {
+                var percent = e.BytesSent * 100 / e.TotalBytesToSend;              
+                Console.WriteLine(percent + "");
+                    progressBar1.Value = (int)percent;
+                    label1.Text = percent.ToString() + "%";
+                
+                    
+            }));
+        }
+
+        private void Client_UploadFileCompleted(object sender, UploadFileCompletedEventArgs e)
+        {
+            var data = Encoding.UTF8.GetString(e.Result);
+            var responseData = JsonConvert.DeserializeObject<ResponseData>(data);
+            if (responseData.status == "SUCCESS")
+            {
+                var link = JsonConvert.DeserializeObject<List<string>>( responseData.data);
+                pictureBox1.LoadAsync(link.FirstOrDefault());
+
+            }
+            else
+            {
+                MessageBox.Show(responseData.message);
+            }
+            
+        }
+
+        public class ResponseData
+        {
+            public string status { get; set; }
+            public string message { get; set; }
+            public string data { get; set; }
+        }
+
+
+    }
+}
---
+++ uploadFilesAPI/uploadFilesAPI/Controllers/UploadFileController.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace uploadFilesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[Action]")]
+    public class UploadFileController : ControllerBase
+    {
+        IWebHostEnvironment _webHost;
+        public UploadFileController(IWebHostEnvironment webHost)
+        {
+            _webHost = webHost;
+        }
+
+        public class ResponseData
+        {
+            public string status { get; set; }
+            public string message { get; set; }
+            public string data { get; set; }
+        }
+        public class FileUploadInfo
+        {
+            public string filename { get; set; }
+            public long filesize { get; set; }
+            public string prettyFileSize
+            {
+                get
+                {
+                    return BytesToReadableValue(this.filesize);
+                }
+            }
+
+            public string BytesToReadableValue(long number)
+            {
+                var suffixes = new List<string> { " B", " KB", " MB", " GB", " TB", " PB" };
+
+                for (int i = 0; i < suffixes.Count; i++)
+                {
+                    long temp = number / (int)Math.Pow(1024, i + 1);
+
+                    if (temp == 0)
+                    {
+                        return (number / (int)Math.Pow(1024, i)) + suffixes[i];
+                    }
+                }
+
+                return number.ToString();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFileAsync(List<IFormFile> file, Guid IDBaiViet)
+        {
+            const bool AllowLimitSize = true;
+            const bool AllowLimitFileType = true;
+
+            var limitFileSize = 2097152; // allow upload file less 2MB = 2097152
+            var listFileError = new List<FileUploadInfo>();
+            var responseData = new ResponseData();
+            string result = "";
+
+            if(file.Count <= 0)
+            {
+                responseData.status = "ERROR";
+                responseData.message = $"Please, select file to upload.";                
+                result = JsonConvert.SerializeObject(responseData);
+                return Ok(result);
+            }
+
+            var listFileTypeAllow = "jpg|png|gif|xls|xlsx";
+            // check file type upload allow
+            if (AllowLimitFileType)
+            {
+                foreach (var formFile in file)
+                {
+                    var file_ext = Path.GetExtension(formFile.FileName).Replace(".", "");
+                    var isAllow = listFileTypeAllow.Split('|').Any(x => x.ToLower() == file_ext.ToLower());
+                    if (!isAllow)
+                    {
+                        listFileError.Add(new FileUploadInfo()
+                        {
+                            filename = formFile.FileName,
+                            filesize = formFile.Length
+                        });
+                    }
+
+                }
+            }
+           
+
+            if (listFileError.Count > 0)
+            {
+                responseData.status = "ERROR";
+                responseData.data = JsonConvert.SerializeObject(listFileError);
+                responseData.message = $"File type upload only Allow Type: ({listFileTypeAllow}) \r\n {responseData.data}";
+                result = JsonConvert.SerializeObject(responseData);
+                return Ok(result);
+            }
+
+
+            // check list file less limit size
+            if (AllowLimitSize)
+            {
+                foreach (var formFile in file)
+                {
+                    if (formFile.Length > limitFileSize)
+                    {
+                        listFileError.Add(new FileUploadInfo()
+                        {
+                            filename = formFile.FileName,
+                            filesize = formFile.Length
+                        });
+                    }
+                }
+            }
+           
+
+            var listLinkUploaded = new List<string>();
+
+            if (listFileError.Count > 0)
+            {
+                responseData.status = "ERROR";
+                responseData.data = JsonConvert.SerializeObject(listFileError);
+                responseData.message = $"File upload must less 2MB ({ responseData.data})";
+                result = JsonConvert.SerializeObject(responseData);
+                return Ok(result);
+            }
+
+            string baseUrl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+            foreach (var formFile in file)
+            {
+
+                if (formFile.Length > 0)
+                {
+
+                    var time = DateTime.UtcNow;
+                    var timestring = $"{time}";
+
+                    string[] charsToRemove = new string[] { "@", ",", ".", ";", "'", "/", ":", " " };
+                    foreach (var c in charsToRemove)
+                    {
+                        timestring = timestring.Replace(c, string.Empty);
+                    }
+                    var templateUrl = Path.GetExtension(formFile.FileName).Replace(".", $"{IDBaiViet}{timestring}.");
+
+                    //var templateUrl = formFile.FileName ;
+                    string filePath = Path.Combine($"{_webHost.WebRootPath}/uploads/", templateUrl);
+                    string fileName = Path.GetFileName(filePath);
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                    listLinkUploaded.Add($"{baseUrl}/uploads/{templateUrl}");
+                }
+            }
+
+            responseData.status = "SUCCESS";
+            responseData.data = JsonConvert.SerializeObject(listLinkUploaded);
+            responseData.message = $"uploaded {file.Count} files successful.";
+            result = JsonConvert.SerializeObject(responseData);
+
+            return Ok(result);
+        }
+    }
+
+
+}
